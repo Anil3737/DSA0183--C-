c6c681c991ca54c2e42f025aa757c11a685b7b85
J.Chiranjevi Anil <132431106+Anil3737@users.noreply.github.com>
2023-12-30 09:38:58 +0530

    Add files via upload

---
+++ Vowel_Not.cpp
@@ -0,0 +1,43 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cout<<"Enter a Letter::";
+	cin>>ch;
+	switch(ch)
+	{
+		case 'a':
+			cout<<"VOWEL::"<<ch;
+			break;
+		case 'e':
+			cout<<"VOWEL::"<<ch;
+			break;
+		case 'i':
+			cout<<"VOWEL::"<<ch;
+			break;
+		case 'o':
+			cout<<"VOWEL::"<<ch;
+			break;
+		case 'u':
+			cout<<"VOWEL::"<<ch;
+			break;
+			case 'A':
+			cout<<"VOWEL::"<<ch;
+			break;
+		case 'E':
+			cout<<"VOWEL::"<<ch;
+			break;
+		case 'I':
+			cout<<"VOWEL::"<<ch;
+			break;
+		case 'O':
+			cout<<"VOWEL::"<<ch;
+			break;
+		case 'U':
+			cout<<"VOWEL::"<<ch;
+			break;
+		default:
+			cout<<"NOT VOWEL::"<<ch;
+	}
+}
